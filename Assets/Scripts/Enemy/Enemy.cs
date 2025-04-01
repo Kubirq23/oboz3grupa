38a95e35ba7a153725e9befb334edf5aa739d617
Reter <defdsdhjvwsacyufheckyuw@gdfjwsdgjs.com>
2025-04-01 17:13:51 +0200

    pathfinding + enemy spawn

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,17 +4,26 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour{
     [SerializeField] Transform target;
 
+    [SerializeField]
+    private bool updatePosition;
     NavMeshAgent agent;
+
+
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
-        agent.updatePosition = false;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     void Update()
     {
         agent.SetDestination(target.position);
+    }
+
+    private void OnValidate()
+    {
+        agent.updateRotation = updatePosition;
     }
 }
