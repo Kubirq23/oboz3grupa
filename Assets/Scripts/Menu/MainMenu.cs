cda0ad934f4a5bac5d55898799c4ac326f5fc2fb
Reter <defdsdhjvwsacyufheckyuw@gdfjwsdgjs.com>
2025-04-02 16:40:03 +0200

    Main Menu
    imi n th thic o fit...

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,3 +1,4 @@
+using Ami.BroAudio;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,27 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+
+
+    }
+
+
+    public void SetMasterVolume(float volume)
+    {
+        BroAudio.SetVolume(volume);
+    }
+
+    public void SetUIVolume(float volume)
+    {
+        BroAudio.SetVolume(BroAudioType.UI, volume);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        BroAudio.SetVolume(BroAudioType.SFX, volume);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        BroAudio.SetVolume(BroAudioType.Music, volume);
     }
 
     public void QuitGame()
