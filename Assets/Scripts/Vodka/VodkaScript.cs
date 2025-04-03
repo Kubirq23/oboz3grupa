6746b6b8d932144f82dfd7f448b1f6bc073d3192
trex90911 <maksymgorski09@gmail.com>
2025-04-03 17:08:40 +0200

    drunkeffect

--- Assets/Scripts/Vodka/VodkaScript.cs
+++ Assets/Scripts/Vodka/VodkaScript.cs
@@ -1,11 +1,51 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class VodkaScript : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collision){
-        if(collision.gameObject.CompareTag("Player")){
+    [SerializeField]
+    private Material coolMaterial;
+    [SerializeField]
+    private Volume volume;
+
+    [SerializeField]
+    private ColorAdjustments adjustements;
+
+    private int maybeDrunk;
+    private Tween tween;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
             collision.gameObject.GetComponent<Player>().Heal(1);
             Destroy(gameObject);
-        }   
+
+            maybeDrunk = Random.Range(0, 3);
+            if (maybeDrunk == 1)
+            {
+                Debug.Log("Drunk");
+                if (tween == null)
+                {
+                    tween = coolMaterial.DOFloat(2f, "_Strength", .5f).SetEase(Ease.InExpo).OnComplete(()=> EndEffect());
+                    DOTween.To(() => volume.weight, x => volume.weight = x, 1f, .5f);
+                }
+            }
+        }
+    }
+
+    private void EndEffect()
+    {
+        if (tween != null)
+        {
+            tween.Kill();
+        }
+        coolMaterial.DOFloat(0f, "_Strength", 10f).SetEase(Ease.OutSine);
+        DOTween.To(() => volume.weight, x => volume.weight = x, 0f, 10f);
+
+    }
+    private void OnDisable()
+    {
+                coolMaterial.SetFloat("_Strength", 0f);
     }
 }
